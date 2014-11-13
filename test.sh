a829fc85cbe7e8017be225cf7f2319a981f6cf54
Matt Gautreau <mgaut72@gmail.com>
2014-11-13 16:44:35 -0700

    add script to test all the things

---
+++ test.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+# expects the executable that is your C-- compile to be passed in as
+# an environment variable
+#
+# Usage: $ COMPILE=/path/to/my/compile MILESTONE=1 test.sh
+#
+#          COMPILE should be set to your c-- compiler executable
+#          MILESTONE should be 1 | 2 | both, default is 1
+
+
+if [ -z ${COMPILE+x} ]
+then
+    echo "expected environment variable COMPILE as your compiler path"
+    echo "ex: COMPILE=/my/compiler.exe test.sh"
+    exit 1
+fi
+
+if [ -z ${MILESTONE+x} ]
+then
+    MILESTONE=1
+fi
+
+if [[ $MILESTONE -ne "both" ]]
+then
+    DIR="./milestone$MILESTONE"
+else
+    DIR="."
+fi
+
+find $DIR -type f -iname "*.c" -print0 | while IFS= read -r -d $'\0' line;
+do
+    echo "testing : $line"
+    make name=$line > /dev/null 2> /dev/null
+    diff <(./a.out && rm -f a.out) <(cat $line | $COMPILE | spim -file /dev/stdin 2> /dev/null | tail -n +6)
+done
